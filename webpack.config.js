fa597473b84a3f8fc80bb3b0572e808c9bb50c86
Armando Sosa <asosa@nolimit-studio.com>
2018-06-13 14:36:35 -0500

    updated server config

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,38 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const port = process.env.PORT || 8000;
 
 module.exports = {
-  entry: path.join(__dirname, "src/docs"),
+  entry: path.join(__dirname, 'src/docs'),
   output: {
-    path: path.join(__dirname, "docs"),
-    filename: "bundle.js"
+    path: path.join(__dirname, 'docs'),
+    filename: 'bundle.js',
   },
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
-        use: "babel-loader",
-        exclude: /node_modules/
+        use: 'babel-loader',
+        exclude: /node_modules/,
       },
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"]
-      }
-    ]
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.join(__dirname, "src/docs/index.html")
-    })
+      template: path.join(__dirname, 'src/docs/index.html'),
+    }),
   ],
   resolve: {
-    extensions: [".js", ".jsx"]
+    extensions: ['.js', '.jsx'],
   },
   devServer: {
-    contentBase: path.join(__dirname, "docs"),
-    port: 8000,
-    stats: "minimal"
-  }
+    contentBase: path.join(__dirname, 'docs'),
+    port,
+    stats: 'minimal',
+  },
 };
