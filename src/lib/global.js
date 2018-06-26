f8ae5b7c0918e37f6313d3d11e07327a3ec7a4c2
Armando Sosa <asosa@nolimit-studio.com>
2018-06-26 17:21:06 -0500

    fixed .gitignore issue

---
+++ src/lib/global.js
@@ -0,0 +1,55 @@
+// @flow
+import * as React from 'react';
+import createListener from 'keyboardist';
+
+const listeners = {
+  keydown: createListener('keydown'),
+  keyup: createListener('keyup'),
+};
+
+type Props = {
+  eventName: 'keydown' | 'keyup',
+  bindings: { [string]: any },
+  monitor?: any,
+};
+
+class Keyboard extends React.PureComponent<Props> {
+  subs = [];
+
+  keyboardListener: any;
+
+  static defaultProps = {
+    eventName: 'keydown',
+    bindings: {},
+    monitor: null,
+  };
+
+  constructor(props: Props) {
+    super(props);
+    this.keyboardListener = listeners[props.eventName];
+  }
+
+  componentDidMount() {
+    const { bindings, monitor } = this.props;
+
+    Object.keys(bindings).forEach(eventName => {
+      const callback = bindings[eventName];
+      const subscription = this.keyboardListener.subscribe(eventName, callback);
+      this.subs.push(subscription);
+    });
+
+    if (monitor) {
+      this.keyboardListener.setMonitor(monitor);
+    }
+  }
+
+  componentWillUnmount() {
+    this.subs.forEach(subscription => subscription.unsubscribe());
+  }
+
+  render() {
+    return null;
+  }
+}
+
+export default Keyboard;
---
+++ src/lib/input.js
@@ -0,0 +1,46 @@
+import * as React from 'react';
+import createListener from 'keyboardist';
+
+class Input extends React.PureComponent {
+  subs = [];
+
+  static defaultProps = {
+    component: 'input',
+    eventName: 'keydown',
+    bindings: {},
+    monitor: null,
+  };
+
+  componentDidMount() {
+    const { bindings, monitor, eventName } = this.props;
+
+    this.keyboardListener = createListener(eventName, this.element);
+
+    Object.keys(bindings).forEach(eventName => {
+      const callback = bindings[eventName];
+      const subscription = this.keyboardListener.subscribe(eventName, callback);
+      this.subs.push(subscription);
+    });
+
+    if (monitor) {
+      this.keyboardListener.setMonitor(monitor);
+    }
+  }
+
+  componentWillUnmount() {
+    this.subs.forEach(subscription => subscription.unsubscribe());
+  }
+
+  render() {
+    const {
+      component: Component,
+      eventName,
+      bindings,
+      monitor,
+      ...props
+    } = this.props;
+    return <Component ref={el => (this.element = el)} {...props} />;
+  }
+}
+
+export default Input;
